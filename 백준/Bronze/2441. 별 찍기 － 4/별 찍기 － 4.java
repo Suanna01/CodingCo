29a02270279aaaa95b099476759a7c3b7d45edc8
Sung-won Yang <86403488+Suanna01@users.noreply.github.com>
2024-09-18 18:05:39 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 64 ms, Memory: 11624 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<i; j++){
+                sb.append(" ");
+            }
+            for (int j = n-i; j>=1; j--){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
