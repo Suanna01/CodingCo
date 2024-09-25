d38a2e3cdcc8dc4b82d2380b8be74c5aacfd6f77
Sung-won Yang <86403488+Suanna01@users.noreply.github.com>
2024-09-25 15:35:38 +0900

    [level 1] Title: 소수 만들기, Time: 135.84 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] nums) {
+        int result = 0;
+        
+        for (int i = 0; i<nums.length-2; i++){
+            for (int j = i+1; j<nums.length-1; j++){
+                for (int k = j+1; k<nums.length; k++){
+                    int sum = nums[i]+nums[j]+nums[k];
+                    int count = 0;
+                    for (int l = 2; l < sum-1; l++){
+                        if (sum%l==0) {
+                            count++;
+                        }
+                    }
+                    if (count==0) result++;
+                }
+            }
+        }
+        return result;
+        
+    }
+}
