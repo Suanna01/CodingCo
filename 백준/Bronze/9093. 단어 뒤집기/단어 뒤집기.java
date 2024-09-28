a5efb5a45a40755737059d44e947231ac1b0d0d8
Sung-won Yang <86403488+Suanna01@users.noreply.github.com>
2024-09-28 21:09:51 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 328 ms, Memory: 49380 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,20 +1,24 @@
 import java.io.*;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
         
         for (int i = 0; i<n; i++){
-            String s = br.readLine();
-            String[] ss = s.split(" ");
-            for (int j = 0; j<ss.length; j++){
-                StringBuilder S = new StringBuilder(ss[j]);
+            // String s = br.readLine();
+            // String[] ss = s.split(" ");
+            st = new StringTokenizer(br.readLine());
+            int count = st.countTokens();
+            for (int j = 0; j<count; j++){
+                StringBuilder S = new StringBuilder(st.nextToken());
                 // for (int k = S.length-1; k>=0; k--){
                 //     sb.append(S[k]);
                 // }
-                sb.append(S.reverse()+" ");
+                sb.append(S.reverse()).append(" ");
             }
             sb.append("\n");
         }
