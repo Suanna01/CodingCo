eb5776bc56b95ac68d53d96133e05429b7b3b6c2
Sung-won Yang <86403488+Suanna01@users.noreply.github.com>
2024-10-08 16:32:32 +0900

    [Update] 백준 - 수 정렬하기 2 - 계수정렬로 풀기

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -10,18 +10,22 @@ public class Main{
         int n = Integer.parseInt(br.readLine());
         
         // int[] a = new int[n];
-        List<Integer> a = new ArrayList<>();
+        // List<Integer> a = new ArrayList<>();
+        boolean[] a = new boolean[2000001];
         
         for (int i = 0; i<n; i++){
             // a[i]=Integer.parseInt(br.readLine());
-            a.add(Integer.parseInt(br.readLine()));
+            a[Integer.parseInt(br.readLine())+1000000] = true;
         }
         
         // Arrays.sort(a);
-        Collections.sort(a);
+        // Collections.sort(a);
         
-        for (int i = 0; i<n; i++){
-            sb.append(a.get(i)+"\n");
+        for (int i = 0; i<a.length; i++){
+            // sb.append(a.get(i)+"\n");
+            if (a[i]){
+                sb.append(i-1000000+"\n");
+            }
         }
         
         bw.write(sb.toString());
