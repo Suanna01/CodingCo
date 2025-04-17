d9f1feacd662d12bf3c4610cf04cfe56fa280d7d
Sung-won Yang <86403488+Suanna01@users.noreply.github.com>
2025-04-17 14:37:19 +0900

    [Silver V] Title: 나이순 정렬, Time: 512 ms, Memory: 47752 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,37 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringBuilder sb = new StringBuilder();
-    
-    StringTokenizer st;
-    
-    int n = Integer.parseInt(br.readLine());
+    // 정렬을 위해 객체 생성
+    static class Person{
+        int age;
+        String name;
+        int order; //입력된 순서
 
-    String[][] a = new String[n][2];
-    
-    for (int i = 0; i<n; i++){
-      st = new StringTokenizer(br.readLine());
-      a[i][0] = st.nextToken();
-      a[i][1] = st.nextToken();
+        //생성자
+        Person(int age, String name, int order){
+            this.age=age;
+            this.name=name;
+            this.order=order;
+        }
     }
 
-    Arrays.sort(a, new Comparator<String[]>() {
-      @Override
-      public int compare(String[] a, String[] b){
-        return Integer.parseInt(a[0]) - Integer.parseInt(b[0]);
-      }
-    });
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-    for (int i = 0; i<n; i++){
-      sb.append(a[i][0]+ " " + a[i][1]+ "\n");
+        //Person 객체 배열 생성
+        Person[] people = new Person[n];
+
+        for (int i = 0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            people[i] = new Person(age, name, i);
+        }
+
+        /*
+        정렬 기준 :
+        1순위. 나이순 정렬
+        2순위. 나이 같으면 입력 순서에 따라 정렬
+        */
+        Arrays.sort(people, new Comparator<Person>() {
+            public int compare(Person p1, Person p2){
+                if (p1.age==p2.age){
+                    return p1.order-p2.order; //나이가 같으면 입력된 순서에 따라 비교
+                }
+                return p1.age-p2.age; //나이순 정렬
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (Person p : people){
+            sb.append(p.age).append(' ').append(p.name).append('\n');
+        }
+
+        System.out.print(sb);
     }
-
-    bw.write(sb.toString());
-    bw.flush();
-    bw.close();
-    br.close();
-  }
 }
