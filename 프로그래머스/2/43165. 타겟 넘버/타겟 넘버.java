b818b475d58018f27b93778551f8fc626effd900
Sung-won Yang <86403488+Suanna01@users.noreply.github.com>
2025-05-16 19:46:05 +0900

    [level 2] Title: 타겟 넘버, Time: 4.76 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,22 @@
 class Solution {
-    int answer = 0, sum = 0;
-    public void dfs(int[] numbers, int target, int index, int sum){
-        index++;
-        if (index==numbers.length){
-            if(sum==target) answer++;
+    int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, 0, 0, target);
+        return answer;
+    }
+    
+    private void dfs(int[] numbers, int index, int sum, int target) {
+        // 종료 조건
+        if (index==numbers.length) {
+            if (sum==target){
+                answer++;
+            }
             return;
         }
         
-        else {
-            dfs(numbers, target, index, sum+numbers[index]);
-            dfs(numbers, target, index, sum-numbers[index]);
-        }
-    }
-    
-    public int solution(int[] numbers, int target) {
-        dfs(numbers, target, -1, 0);
-        
-        return answer;
+        // 재귀 호출: +, -
+        dfs(numbers, index+1, sum+numbers[index], target);
+        dfs(numbers, index+1, sum-numbers[index], target);
     }
 }
