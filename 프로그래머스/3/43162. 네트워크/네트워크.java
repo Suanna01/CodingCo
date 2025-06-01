292e4feb24a255eea80260ed7932adc5235bed11
Sung-won Yang <86403488+Suanna01@users.noreply.github.com>
2025-06-01 17:12:03 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,22 +1,33 @@
 class Solution {
-    public void dfs(int node, boolean[] visited, int[][] computers){
-        visited[node] = true;
-        for (int i = 0; i<computers.length; i++){
-            if (!visited[i] && computers[node][i]==1) {
-                dfs(i, visited, computers);
-            }
-        }
-    }
+    // dfs
+    // 네방향 아님
+    // '나'면 탐색 안해야함 (방문여부, 1인지 여부 + 나인지 여부)
+    // 그룹 개수 리턴임(단, 혼자인거도 그룹 +1)
     
-    public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];
+    // 그래프!
+    // n 주어져 있었음. 입출력 확실히 보기
+        
+    public int solution(int n, int[][] computers) {    
         int answer = 0;
-        for(int i = 0; i<n; i++) {
-            if (!visited[i]){
-                dfs(i, visited, computers);
+        boolean[] visited =  new boolean[n];
+        
+        for (int i = 0; i < n; i++){
+            if (!visited[i]) {
+                dfs(i, n, computers, visited);
                 answer++;
             }
         }
+        
         return answer;
+    }
+    
+    private void dfs(int node, int n, int[][] computers, boolean[] visited){
+        visited[node] = true;
+        
+        for (int i = 0; i < n; i++){
+            if (!visited[i] && computers[node][i] == 1 && i != node) {
+                dfs(i, n, computers, visited);
+            }
+        }
     }
 }
